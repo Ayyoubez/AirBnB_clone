1828b89145c9f21109d18c4e831ede2eadc6995f
ayyoubez <ayyoubez@gmail.com>
2024-03-08 13:18:57 +0100

    missing commit

--- models/base_model.py
+++ models/base_model.py
@@ -15,6 +15,9 @@ class BaseModel:
             *args (dif): unused.
             **kwargs (dict): key/value as a dictionnary.
         """
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         if len(kwargs) != 0:
             for key, value in kwargs.items():
@@ -24,12 +27,9 @@ class BaseModel:
                     else:
                         self.__dict__[key] = value
         else:
-            self.id = str(uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
             models.storage.new(self)
 
-    def __str__(slef):
+    def __str__(self):
         """Return the string representation of the BaseModel instances"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
@@ -45,3 +45,4 @@ class BaseModel:
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
+
